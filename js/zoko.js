9fc6ef9cc1b56604d57dceff4530a11dfc1aff86
Sebastian Viklund <zyxster@gmail.com>
2012-11-16 19:44:52 +0100

    Removed unnecessary lines from Mesh.coffee

--- js/zoko.js
+++ js/zoko.js
@@ -154,9 +154,6 @@ e3d.Mesh = function(data) {
   program = e3d.program.mesh;
   vertexbuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexbuffer);
-  gl.vertexAttribPointer(program.aPositionLoc, 3, gl.FLOAT, false, 32, 0);
-  gl.vertexAttribPointer(program.aTexCoordLoc, 2, gl.FLOAT, false, 32, 12);
-  gl.vertexAttribPointer(program.aColorLoc, 3, gl.FLOAT, false, 32, 20);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
   nvertices = data.length / 8;
   this.render = function() {
